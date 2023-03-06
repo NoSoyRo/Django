39044025cc56e36875c97b556c527e7b17e23a2d
José Rodrigo Moreno López <60509841+NoSoyRo@users.noreply.github.com>
2023-03-06 07:44:26 -0600

    Update settings.py

--- practice_problems/problem001/sheriff/sheriff/settings.py
+++ practice_problems/problem001/sheriff/sheriff/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x7orz86a++!*583@6=ms9t*p*5_0qzugj88bci0!4s@37pei94'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
