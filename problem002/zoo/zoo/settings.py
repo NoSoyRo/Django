ef48101a990705298005abe66330815bf4215249
José Rodrigo Moreno López <60509841+NoSoyRo@users.noreply.github.com>
2023-03-06 19:16:03 -0600

    Update settings.py

--- problem002/zoo/zoo/settings.py
+++ problem002/zoo/zoo/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g(1x2&d)1px3wpzuj=ko5-k36gbq+x6h+43eo^s10)jf-kcap1'
+# SECURITY WARNING: keep the secret key used in production secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,10 +77,7 @@ DATABASES = {
     'default': {  
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'zoo',  
-        'USER': 'root',  
-        'PASSWORD': 'mapayoli78',  
-        'HOST': '127.0.0.1',  
-        'PORT': '3306'
+        'USER': 'root', 
     }  
 }  
 
